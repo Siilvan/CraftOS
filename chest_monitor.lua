8c6f5f70c48926b2910bef72959dd26993626f73
Silvan Groenveld <151916823+Siilvan@users.noreply.github.com>
2024-05-13 16:22:31 +0200

    Add files via upload

---
+++ chest_monitor.lua
@@ -0,0 +1,27 @@
+function print_inventory_on_monitor(chest, monitor)
+    for slot, item in pairs(chest.list()) do
+        writeWithEnter(monitor, item.name, item.count .. "x")
+    end
+end
+
+local function centerText(monitor, text)
+    x,y = monitor.getSize()
+    x1,y1 = monitor.getCursorPos()
+    monitor.setCursorPos((math.floor(x/2) - (math.floor(#text/2))), y1 + 1)
+    monitor.write(text)
+end
+function writeWithEnter(monitor, item, quantity)
+    monitor.setTextColour(1)
+    centerText(monitor, item)
+    monitor.setTextColour(256)
+    centerText(monitor, quantity)
+end
+
+while true do
+    local monitor = peripheral.find("monitor")
+    local chest = peripheral.find("chest")
+
+    print_inventory_on_monitor(chest, monitor)
+
+    sleep(10)
+end
