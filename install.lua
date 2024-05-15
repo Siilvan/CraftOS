5b75f7112fc1496c7d91565730e9180b0fb6eefe
Silvan Groenveld <151916823+Siilvan@users.noreply.github.com>
2024-05-15 13:30:11 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,5 +1,35 @@
-local url1 = "https://raw.githubusercontent.com/Siilvan/CraftOS/main/chest_monitor.lua"
-local url2 = "https://raw.githubusercontent.com/Siilvan/CraftOS/main/startup.lua"
+function downloadFile(url, filename)
+    local response = http.get(url)
+    if response then
+        local file = fs.open(filename, "w")
+        file.write(response.readAll())
+        file.close()
+        response.close()
+        return true
+    else
+        print("Kon het bestand niet downloaden van " .. url)
+        return false
+    end
+end
 
-os.execute("wget " .. url1)
-os.execute("wget " .. url2)
+local githubUrl = "https://raw.githubusercontent.com/Siilvan/CraftOS/main/chest_monitor.lua"
+local localFilename = "chest_monitor.lua"
+
+print("Bezig met downloaden van chest_monitor.lua...")
+if downloadFile(githubUrl, localFilename) then
+    print("Het bestand is succesvol gedownload als " .. localFilename)
+else
+    error("Fout bij het downloaden van chest_monitor.lua")
+end
+
+local startupContent = "shell.run(\"" .. localFilename .. "\")"
+local startupFilename = "startup.lua"
+
+local startupFile = fs.open(startupFilename, "w")
+startupFile.write(startupContent)
+startupFile.close()
+print("startup.lua is aangemaakt.")
+
+print("De computer zal nu herstarten om de chest monitor automatisch te starten.")
+os.sleep(2)
+os.reboot()
