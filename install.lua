9189bfd9047dca1041399bde64c8a89eb251f65c
Silvan Groenveld <151916823+Siilvan@users.noreply.github.com>
2024-05-15 13:11:40 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,3 +1,5 @@
-wget https://raw.githubusercontent.com/Siilvan/CraftOS/main/chest_monitor.lua
-sleep(4)
-wget https://raw.githubusercontent.com/Siilvan/CraftOS/main/startup.lua
+local url1 = "https://raw.githubusercontent.com/Siilvan/CraftOS/main/chest_monitor.lua"
+local url2 = "https://raw.githubusercontent.com/Siilvan/CraftOS/main/startup.lua"
+
+os.execute("wget " .. url1)
+os.execute("wget " .. url2)
